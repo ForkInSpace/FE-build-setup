827e6892a38732c3011226f6237b18fd5c1ee2f5
Zaza T <ts.zaza@gmail.com>
2018-02-21 13:36:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './index.css';
 
 import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+const proValue = numeral(4000).format('$0,0.00');
+console.log(`I would pay ${proValue} for this work!`); // eslint-disable-line no-console
